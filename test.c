e649480f45832779d09194d7d93863dd60ad99aa
Michael Haberler <git@mah.priv.at>
2014-04-11 23:52:05 +0200

    adapt example

--- test.c
+++ test.c
@@ -9,45 +9,42 @@
 
 void test_bufring(void)
 {
+        int i;
 	bringbuffer_t ring1 = { .write = 0, .read = 0 };
 	bringbuffer_t ring2 = { .write = 0, .read = 0 };
 	bringbuffer_t *ro = &ring1, *rw = &ring2;
 
+	ringvec_t tx[3] = {
+	    { .rv_base = "part0", .rv_len = 5, .rv_flags = 1},
+	    { .rv_base = "part1", .rv_len = 5, .rv_flags = 2},
+	    { .rv_base = "part2", .rv_len = 5, .rv_flags = 3},
+	};
+
 	ring_init(&ro->ring, 1024, 0);
 	ring_init(&rw->ring, 0, ro->ring.header);
 
-	bring_write(rw, "part0", 5, 1);
-	bring_write(rw, "part1", 5, 2);
-
-	//ring_dump(&ro->ring, "ro"); ring_dump(&rw->ring, "rw");
-
-	bring_write(rw, "part2", 5, 3);
+	for (i = 0; i < 3; i++) {
+	    bring_write(rw, &tx[i]);
+	}
 	bring_write_flush(rw);
 
-	//ring_dump(&ro->ring, "ro"); ring_dump(&rw->ring, "rw");
+	int n;
+	ringvec_t rx[10] = { };
 
-	const void * ptr;
-	size_t size;
-	int flags;
-	bring_read(ro, &ptr, &size, &flags);
-	printf("Data: %zd %.*s\n", size, (int) size, (const char *) ptr);
-	bring_shift(ro);
-	bring_read(ro, &ptr, &size, &flags);
-	printf("Data: %zd %.*s\n", size, (int) size, (const char *) ptr);
-	bring_shift(ro);
-	bring_read(ro, &ptr, &size, &flags);
-	printf("Data: %zd %.*s\n", size, (int) size, (const char *) ptr);
-	bring_shift(ro);
+ 	for (n = 0; bring_read(ro, &rx[n]) == 0; n++, bring_shift(ro));
 
-	//ring_dump(ro, "ro"); ring_dump(rw, "rw");
-
+	for (i = 0; i < n; i++) {
+	    ringvec_t *v = &rx[i];
+	    printf("Data[%d]: %zd '%.*s' %d\n", i, v->rv_len, (int) v->rv_len,
+		   (const char *) v->rv_base, v->rv_flags);
+	}
 	bring_shift_flush(ro);
 }
 
 int main(void)
 {
 	test_bufring();
-
+#if 0
 	ringbuffer_t ring1, ring2;
 	ringbuffer_t *ro = &ring1, *rw = &ring2;
 	ring_init(ro, 1024, 0);
@@ -66,7 +63,7 @@ int main(void)
 
 	ring_shift(ro);
 	ring_dump(ro, "ro"); ring_dump(rw, "rw");
-
+#endif
 	exit(0);
 }
 
