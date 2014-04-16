70a6e738ca0f141e97ce93a2f0485a996662008a
Pavel Shramov <psha@lebu.psha.org.ru>
2014-04-16 22:30:27 +0400

    pyring: python3 compatibility

--- pyring.pyx
+++ pyring.pyx
@@ -2,10 +2,22 @@
 
 from libc.errno cimport EAGAIN
 from libc.string cimport memcpy
-from cpython.oldbuffer cimport PyBuffer_FromMemory
-from cpython.string cimport PyString_AsString, PyString_Size
+from cpython.buffer cimport PyBuffer_FillInfo
+from cpython.bytes cimport PyBytes_AsString, PyBytes_Size, PyBytes_FromStringAndSize
 
 from libring cimport *
+
+cdef class mview:
+    cdef void *base
+    cdef int size
+
+    def __cinit__(self, long base, size):
+        self.base = <void *>base
+        self.size = size
+
+    def __getbuffer__(self, Py_buffer *view, int flags):
+        r = PyBuffer_FillInfo(view, self, self.base, self.size, 0, flags)
+        view.obj = self
 
 cdef class Ring:
     cdef ringbuffer_t _ring
@@ -19,13 +31,13 @@ cdef class Ring:
 
     def write(self, s):
         cdef void * ptr
-        cdef size_t size = PyString_Size(s)
+        cdef size_t size = PyBytes_Size(s)
         cdef int r = ring_write_begin(&self._ring, &ptr, size)
         if r:
             if r != EAGAIN:
                 raise RuntimeError("Ring write failed")
             return False
-        memcpy(ptr, PyString_AsString(s), size)
+        memcpy(ptr, PyBytes_AsString(s), size)
         ring_write_end(&self._ring, ptr, size)
         return True
 
@@ -37,7 +49,7 @@ cdef class Ring:
             if r != EAGAIN:
                 raise RuntimeError("Ring read failed")
             return None
-        return PyBuffer_FromMemory(ptr, size)
+        return memoryview(mview(<long>ptr, size))
 
     def shift(self):
         ring_shift(&self._ring)
@@ -60,7 +72,7 @@ cdef class RingIter:
             if r != EAGAIN:
                 raise RuntimeError("Ring read failed")
             return None
-        return PyBuffer_FromMemory(ptr, size)
+        return memoryview(mview(<long>ptr, size))
 
     def shift(self):
         ring_iter_shift(&self._iter)
@@ -72,7 +84,7 @@ cdef class RingIter:
         r = self.read()
         if r is None:
             raise StopIteration("Ring is empty")
-        s = str(r)
+        s = memoryview(r.tobytes())
         self.shift()
         return s
 
