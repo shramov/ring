fedf383ebd28a2d7bfb125ed09b41ed64d57049a
Pavel Shramov <psha@lebu.psha.org.ru>
2014-04-16 22:31:05 +0400

    pyring: BufRing support

--- libring.pxd
+++ libring.pxd
@@ -26,3 +26,24 @@ cdef extern from "ring.h":
     int ring_iter_shift(ringiter_t *iter)
     int ring_iter_read(const ringiter_t *iter, const void **data, size_t *size)
 
+cdef extern from "bufring.h":
+    ctypedef struct bringbuffer_t:
+        ringbuffer_t * ring
+
+    ctypedef struct ringvec_t:
+        void  *rv_base
+        size_t rv_len
+        int    rv_flags
+
+    int bring_write_begin(bringbuffer_t *ring, void ** data, size_t size, int flags)
+    int bring_write_end(bringbuffer_t *ring, void * data, size_t size)
+    int bring_write(bringbuffer_t *ring, const void * data, size_t size, int flags)
+    int bring_writev(bringbuffer_t *ring, ringvec_t *rv)
+    int bring_write_flush(bringbuffer_t *ring)
+    int bring_write_abort(bringbuffer_t *ring)
+
+    int bring_read(bringbuffer_t *ring, const void **data, size_t *size, int *flags)
+    int bring_readv(bringbuffer_t *ring, ringvec_t *rv)
+    int bring_shift(bringbuffer_t *ring)
+    int bring_read_flush(bringbuffer_t *ring)
+    int bring_read_abort(bringbuffer_t *ring)
--- pyring.pyx
+++ pyring.pyx
@@ -89,3 +89,52 @@ cdef class RingIter:
         return s
 
     def next(self): return self.__next__()
+
+cdef class ringvec:
+    cdef mview data
+    cdef int flags
+
+    def __cinit__(self, long base, size, flags):
+        self.data = mview(base, size)
+        self.flags = flags
+
+    property data:
+        def __get__(self):
+            return memoryview(self.data)
+
+    property flags:
+        def __get__(self): return self.flags
+
+cdef class BufRing:
+    cdef bringbuffer_t _ring
+    cdef object _pyring
+
+    def __cinit__(self, ring):
+        self._pyring = ring
+        self._ring.ring = &(<Ring>ring)._ring
+
+    def __init__(self, ring):
+        pass
+
+    def read(self):
+        cdef ringvec_t rv
+        bring_read_abort(&self._ring)
+        while bring_readv(&self._ring, &rv) == 0:
+            yield ringvec(<long>rv.rv_base, rv.rv_len, rv.rv_flags)
+            bring_shift(&self._ring)
+
+    def shift(self):
+        bring_read_flush(&self._ring)
+
+    def write(self, s, flags = 0):
+        cdef void * ptr
+        cdef size_t size = PyBytes_Size(s)
+        r = bring_write(&self._ring, PyBytes_AsString(s), size, flags)
+        if not r:
+            return True
+        if r != EAGAIN:
+            raise RuntimeError("Ring write failed")
+        return False
+
+    def flush(self):
+        bring_write_flush(&self._ring)
